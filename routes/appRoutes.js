f51208b1a61b57cf9c4e50e411202b2233301c00
Kiyotaka-Ayanak0ji <101034731+Kiyotaka-Ayanak0ji@users.noreply.github.com>
2024-02-09 11:12:08 +0530

    reports

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -108,7 +108,7 @@ router.get(
   connectEnsureLogin.ensureLoggedIn(),
   async (req, res) => {
     if (req.isAuthenticated()) {
-      if (request.userType === "educator") {
+      if (req.userType === "educator") {
         let myCourses = await Course.findAll({
           where: { userId: req.user.id },
           include: User,
